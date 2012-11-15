92efcdb816977cdfa48597fbb583856fd766f8ee
isyiwang <isaacwang@gmail.com>
2012-11-15 10:59:58 -0800

    Add SwipeJS library

--- src/assets/js/application.js
+++ src/assets/js/application.js
@@ -1,5 +1,14 @@
-$(window).load({
-
+$(window).load(function() {
   var contentWidth = 11.0 / 8.5 * window.innerHeight;
   var navWidth = (window.innerWidth - contentWidth) / 2;
+
+  window.mySwipe = new Swipe(document.getElementById('myCarousel'));
+
+  $('a.left').click(function() {
+    window.mySwipe.prev();
+  });
+
+  $('a.right').click(function() {
+    window.mySwipe.next();
+  })
 });
