581a78aa5b2524a08b95cb3da5c4068eab4dc2b1
Taiei <taieiharimoto@gmail.com>
2012-11-17 20:47:39 -0800

    Arrows

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,9 +1,22 @@
 $(window).load(function() {
   window.swiper = new Swipe(document.getElementById('carousel'));
+  var arrowOpacity = $('a.left').css('opacity');
+
+  var contentWidth = 11.0 / 8.5 * window.innerHeight;
+  var navWidth = (window.innerWidth - contentWidth) / 2;
 
   $('a.left').click(function() {
     window.swiper.prev();
   });
+
+  $('a.left, a.right').hover(
+    function() {
+      $(this).css('opacity', 1.0);
+    },
+    function() {
+      $(this).css('opacity', arrowOpacity);
+    }
+  );
 
   $('a.right').click(function() {
     window.swiper.next();
